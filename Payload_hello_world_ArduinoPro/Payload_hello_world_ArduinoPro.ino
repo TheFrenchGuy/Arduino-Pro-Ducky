9a39d792b5fdb8c99cfe9b5c9860166c95b62eff
TheFrenchGuy <30523707+TheFrenchGuy@users.noreply.github.com>
2017-08-01 08:17:04 +0100

    Payload hello world
    A payload for testing the USB Rubber ducky’s functionality.

---
+++ Payload_hello_world_ArduinoPro/Payload_hello_world_ArduinoPro.ino
@@ -0,0 +1,48 @@
+/*
+ * Generated with <3 by Dckuino.js, an open source project !
+ */
+
+#include "Keyboard.h"
+
+void typeKey(int key)
+{
+  Keyboard.press(key);
+  delay(50);
+  Keyboard.release(key);
+}
+
+/* Init function */
+void setup()
+{
+  // Begining the Keyboard stream
+  Keyboard.begin();
+
+  // Wait 500ms
+  delay(500);
+
+  delay(3000);
+
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+
+  delay(500);
+
+  Keyboard.print("notepad");
+
+  delay(500);
+
+  typeKey(KEY_RETURN);
+
+  delay(750);
+
+  Keyboard.print("Hello World!!!");
+
+  typeKey(KEY_RETURN);
+
+  // Ending stream
+  Keyboard.end();
+}
+
+/* Unused endless loop */
+void loop() {}
